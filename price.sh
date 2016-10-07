7405e06a99850f9e3ac4e5d299347ab5edd2c022
Trevor <tkmday@uw.edu>
2016-10-07 15:25:12 -0700

    get prices a better way

--- price.sh
+++ price.sh
@@ -11,8 +11,8 @@ parser.add_argument("length", type=int, help="Job execution time, on one " + \
     "brain in hours on a neuron-class machine (adrc, tpp, panuc), " + \
     "single-threaded")
 parser.add_argument("num", type=int, help="How many jobs you have.", default=1)
-parser.add_argument("size", type=int, 
-    help="Estimate how large the volume needs to be, in GB.")
+# parser.add_argument("size", type=int, 
+#     help="Estimate how large the volume needs to be, in GB.")
 
 group=parser.add_mutually_exclusive_group(required=True)
 group.add_argument("-F", "--freesurfer", action="store_true", 
@@ -24,7 +24,19 @@ group.add_argument("-P", "--probtrackx", action="store_true",
 group.add_argument("-N", "--neurosim", action="store_true",
     help="Estimate for neurosim.")
 
+output=parser.add_mutually_exclusive_group(required=True)
+output.add_argument("-Q", "--quickest", action="store_true", 
+    help="Get the quickest cluster configuration.")
+output.add_argument("-C", "--cheapest", action="store_true",
+    help="Get the cheapest cluster configuration.")
+output.add_argument("-A", "--all", action="store_true",
+    help="Get all configurations.")
+
 args = parser.parse_args()
+
+if args.all:
+    args.quickest = True
+    args.cheapest = True
 
 if args.freesurfer:
 	program="freesurfer"
@@ -36,6 +48,7 @@ elif args.neurosim:
     program="neurosim"
 
 if args.bedpostx or args.probtrackx:
-   print(best_price(program, args.length, args.num, gpu=True))
-else:
-    print(best_price(program, args.length, args.num))
+    gpu = True
+
+print(best_price(program, args.length, args.num, quick=args.quickest,
+    cheapest=args.cheapest, gpu=gpu))
--- price_instances.py
+++ price_instances.py
@@ -1,7 +1,13 @@
+# Do float devision by default.
+from __future__ import division, print_function
+
 import boto3
-from datetime import datetime, date, time, timedelta
 import numpy as np
 import sys
+import math
+
+from datetime import datetime, date, time, timedelta
+
 
 instances = ['m4.large', 'm4.xlarge', 'c4.xlarge', 'c4.8xlarge'
              #            'm3.medium','m3.large','m3.xlarge','m3.2xlarge',
@@ -18,9 +24,9 @@ instances = ['m4.large', 'm4.xlarge', 'c4.xlarge', 'c4.8xlarge'
 
 gpu_instances = ['g2.2xlarge', 'g2.8xlarge']
 
-regions = ['us-east-1', 'us-west-2', 'us-west-1', 'eu-west-1', 'eu-central-1',
-           'ap-southeast-1', 'ap-northeast-1', 'ap-southeast-2',
-           'ap-northeast-2', 'ap-south-1', 'sa-east-1']
+regions = ['us-east-1', 'us-west-2', 'us-west-1' , 'eu-west-1'] #, 'eu-central-1',
+           #'ap-southeast-1', 'ap-northeast-1', 'ap-southeast-2',
+           #'ap-northeast-2', 'ap-south-1', 'sa-east-1']
 
 # us-e1 N.Virginia; us-w2 Oregon; us-w1 N. California; eu-w1 Ireland;
 # eu-c1 Frankfurt; ap-se1 Singapore; ap-ne1 tokyo, ap-se2 Sydney, ap-ne2 Seoul;
@@ -32,8 +38,7 @@ now = datetime.now()
 five_days_ago = now - five_days
 
 # Experimentally determined speed up times.
-speed_up = {"freesurfer": 1.3, "bedpostx": 161, "probtrackx": 14.9,
-            "neurosim": 1}
+gpu_speed_up = {"bedpostx": 161, "probtrackx": 14.9}
 
 # Numer of cpus per instance
 ncpus = {"t1.micro": 1, "t2.micro": 1, "t2.small": 1, "t2.medium": 2,
@@ -50,38 +55,80 @@ ncpus = {"t1.micro": 1, "t2.micro": 1, "t2.small": 1, "t2.medium": 2,
          "r3.8xlarge": 16, "i2.xlarge": 2, "i2.2xlarge": 4, "i2.4xlarge": 8, 
          "i2.8xlarge": 16, "d2.xlarge": 2, "d2.2xlarge": 4, "d2.4xlarge": 8, 
          "d2.8xlarge": 18,
-         'g2.2xlarge': 1, 'g2.8xlarge': 1  
-         # Not actual cpus, but no gpu concurrency
+         'g2.2xlarge': 1, 'g2.8xlarge': 4  
+         # Not actual cpus, but gpu concurrency = 4
          }
 
-
-def price_instance(instancename, region):
-    """Return the mean price for an instance in a region over the last
-    five days"""
-
+def get_all_in_region(region):
     client = boto3.client("ec2", region)
     response = client.describe_spot_price_history(
-        InstanceTypes=[instancename],
-        StartTime=five_days_ago,
-        EndTime=now,
-        ProductDescriptions=['Linux/UNIX'],
-    )
-    prices = [float(x['SpotPrice']) for x in response['SpotPriceHistory']]
+            InstanceTypes=instances,
+            StartTime=five_days_ago,
+            EndTime=now,
+            ProductDescriptions=['Linux/UNIX'],
+        )
 
-    if (len(prices) > 0):
-        return(np.mean(prices))
-    else:
-        return(sys.maxint)
+    prices = np.empty([0, 3])
+
+    for item in response["SpotPriceHistory"]:
+        mat = np.array([[item["InstanceType"], item["SpotPrice"], 
+            item["AvailabilityZone"]]])
+        prices = np.append(prices, mat, axis=0)
+
+    return(prices)
+
+def get_all_regions():
+    all_prices = np.empty([0, 3])
+    
+    for r in regions:
+        mat = get_all_in_region(r)
+        all_prices = np.append(all_prices, mat, axis=0)
+
+    return(all_prices)
 
 
-def best_price(program, length, num, gpu=False):
+# def price_instance(instancename, region):
+#     """Return the mean price for an instance in a region over the last
+#     five days"""
+
+
+
+#     client = boto3.client("ec2", region)
+#     response = client.describe_spot_price_history(
+#         InstanceTypes=[instancename],
+#         StartTime=five_days_ago,
+#         EndTime=now,
+#         ProductDescriptions=['Linux/UNIX'],
+#     )
+#     prices = [float(x['SpotPrice']) for x in response['SpotPriceHistory']]
+
+#     if (len(prices) > 0):
+#         price = np.mean(prices)
+#     else:
+#         price = sys.maxint
+
+#     return([instancename, region, price])
+
+def make_a_cluster(row, num, length):
+
+    cores = ncpus[row[0]]
+    machines = math.ceil(num / cores)
+    price = row[1]
+
+    print(row)
+    total_cost = machines * length * price
+
+    return([row[0], row[2], price])
+
+def best_price(program, length, num, quick, cheapest, gpu=False):
     """Find the best instance by comparing against previous best."""
 
-    exec_time = length / speed_up[program]
-    print("AWS speed up for " + program + " is " + str(speed_up[program]) +
+    print("AWS speed up for " + program + " is " + str(gpu_speed_up[program]) +
           "x.")
 
+    # For comparison, so the first one is always smaller.
     max_price = sys.maxint
+    max_time = sys.maxint
 
     if gpu:
         search_instances = instances + gpu_instances
@@ -89,25 +136,32 @@ def best_price(program, length, num, gpu=False):
         search_instances = instances
 
     print("Pricing " + str(len(search_instances)) + " instances against " +
-          "all regions ...")
-    print("Result: instance, region, $/hr, total cost, n cpus")
+          str(len(regions)) + " regions ...")
+
     for r in regions:
         for i in search_instances:
-            price = price_instance(i, r)
-            execution_price = price * exec_time * num / ncpus[i]
-            stats = [i, r, price, execution_price, ncpus[i]]
-            if execution_price < max_price:
-                max_price = execution_price
-                best_value = stats
+            if i in gpu_instances:
+                exec_time = length / gpu_speed_up[program]
+            else:
+                exec_time = length 
 
-    print("\nIt will cost you $" + "%.2f" % best_value[3] + " to run " +
-          str(num) + " brains" " on a " + best_value[0] +
-          "-class machine in the " + best_value[1] + " region. It will take " +
-          "approximately " + "%.2f" % (exec_time * num / ncpus[i]) + 
-          " hours.\n")
+            cluster = make_a_cluster(price_instance(i, r), num, exec_time)
 
-    return(best_value)
+            if cluster[4] < max_price:
+                max_price = cluster[4]
+                best_price = cluster
+
+            if cluster[5] < max_time:
+                max_time = cluster[5]
+                best_time = cluster
+
+    print("Format: machine, region, $/instance/hr, #instances, total$, " + 
+        "time(hr)")
 
 
-#print("Price on this machine:")
-#print(args.length/exec_time * args.num * args.length)
+    # Return appropriate things here
+
+every = get_all_regions()
+
+[ make_a_cluster(x, 10, 100) for x in every ]
+
