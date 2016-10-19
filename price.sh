886feb3ba2fbfaaea733c4658bc7377215ab4baa
Trevor <tkmday@uw.edu>
2016-10-19 15:50:05 -0700

    Add -S option

--- price.sh
+++ price.sh
@@ -1,18 +1,34 @@
 #!/usr/bin/env python
 
 import argparse
-from price_instances import best_price
+from price_instances import get_best_cluster # best_price
 
 parser = argparse.ArgumentParser(description="Determine the best value for a \
     given program based on execution time.")
 
-parser.add_argument("name", help="What to name this cluster.")
+# parser.add_argument("name", help="What to name this cluster.")
 parser.add_argument("length", type=int, help="Job execution time, on one " + \
     "brain in hours on a neuron-class machine (adrc, tpp, panuc), " + \
     "single-threaded")
 parser.add_argument("num", type=int, help="How many jobs you have.", default=1)
 # parser.add_argument("size", type=int, 
 #     help="Estimate how large the volume needs to be, in GB.")
+
+parser.add_argument("-s", "--show-all", action="store_true", 
+    help="Show info for all possible clusters.")
+
+parser.add_argument("-S", "--show-all-only", action="store_true",
+    help="Show info for all possibly clusters only (i.e. do not show best " + \
+    "configuration")
+
+parser.add_argument("-v", "--verbose", action="store_true",
+    help="Be more verbose (turns on \`show all\')")
+
+parser.add_argument("-n", "--no-gpus", action="store_true",
+    help="Don't calculate for GPU instances.")
+
+parser.add_argument("-p", "--plaintext", action="store_true",
+    help="Show results parsed into plain English.")
 
 group=parser.add_mutually_exclusive_group(required=True)
 group.add_argument("-F", "--freesurfer", action="store_true", 
@@ -24,31 +40,61 @@ group.add_argument("-P", "--probtrackx", action="store_true",
 group.add_argument("-N", "--neurosim", action="store_true",
     help="Estimate for neurosim.")
 
-output=parser.add_mutually_exclusive_group(required=True)
-output.add_argument("-Q", "--quickest", action="store_true", 
-    help="Get the quickest cluster configuration.")
-output.add_argument("-C", "--cheapest", action="store_true",
-    help="Get the cheapest cluster configuration.")
-output.add_argument("-A", "--all", action="store_true",
-    help="Get all configurations.")
+# output=parser.add_mutually_exclusive_group(required=True)
+# output.add_argument("-Q", "--quickest", action="store_true", 
+#     help="Get the quickest cluster configuration.")
+# output.add_argument("-C", "--cheapest", action="store_true",
+#     help="Get the cheapest cluster configuration.")
+# output.add_argument("-A", "--all", action="store_true",
+#     help="Get all configurations.")
 
 args = parser.parse_args()
 
-if args.all:
-    args.quickest = True
-    args.cheapest = True
+if args.verbose:
+    print(args)
+    args.show_all = True
+
+# if args.all:
+#     args.quickest = True
+#     args.cheapest = True
 
 if args.freesurfer:
-	program="freesurfer"
+    program = "freesurfer"
 elif args.bedpostx:
-	program="bedpostx"
+    program = "bedpostx"
 elif args.probtrackx:
-	program="probtrackx"
+    program = "probtrackx"
 elif args.neurosim:
-    program="neurosim"
+    program = "neurosim"
 
-if args.bedpostx or args.probtrackx:
+# Set gpu status
+if args.no_gpus:
+    # no gpus if no-gpus flag set
+    gpu = False
+elif args.bedpostx or args.probtrackx:
+    # if flag not set, and using *x program, gpus
     gpu = True
+else:
+    # freesurfer/neurosim: don't use gpu 
+    gpu = False
 
-print(best_price(program, args.length, args.num, quick=args.quickest,
-    cheapest=args.cheapest, gpu=gpu))
+# print args if verbose is on
+if args.verbose:
+    print(args)
+
+# show all if -s or -S is set
+if args.show_all or args.show_all_only:
+    showall=True
+else:
+    showall=False
+
+
+# print(best_price(program, args.length, args.num, quick=args.quickest,
+    # cheapest=args.cheapest, gpu=gpu))
+
+get_best_cluster(args.num, args.length, 
+    gpu = gpu,
+    program = program,
+    showall = showall,
+    text = args.plaintext,
+    showbest = not args.show_all_only)
--- price_instances.py
+++ price_instances.py
@@ -5,6 +5,7 @@ import boto3
 import numpy as np
 import sys
 import math
+import re
 
 from datetime import datetime, date, time, timedelta
 
@@ -32,6 +33,9 @@ regions = ['us-east-1', 'us-west-2', 'us-west-1' , 'eu-west-1'] #, 'eu-central-1
 # eu-c1 Frankfurt; ap-se1 Singapore; ap-ne1 tokyo, ap-se2 Sydney, ap-ne2 Seoul;
 # ap-s1 Mumbai; sa-e1 Sao Paulo
 
+# header for printing results
+header=["instance", "region", "total-$", "num", "exec-time", "$/hr", "#cores"]
+
 # Create five days ago datetime object.
 five_days = timedelta(5)
 now = datetime.now()
@@ -40,29 +44,24 @@ five_days_ago = now - five_days
 # Experimentally determined speed up times.
 gpu_speed_up = {"bedpostx": 161, "probtrackx": 14.9}
 
-# Numer of cpus per instance
-ncpus = {"t1.micro": 1, "t2.micro": 1, "t2.small": 1, "t2.medium": 2,
-         "t2.large": 2, "m1.small": 1, "m1.medium": 1, "m1.large": 2, 
-         "m1.xlarge": 4,"m2.xlarge": 2, "m2.2xlarge": 4, "m2.4xlarge": 8, 
-         "m3.medium": 1,"m3.large": 1, "m3.xlarge": 2, "m3.2xlarge": 4, 
-         "m4.large": 1, "m4.xlarge": 2, "m4.2xlarge": 4, "m4.4xlarge": 8, 
-         "m4.10xlarge": 20, "c1.medium": 2, "c1.xlarge": 8, "cc2.8xlarge": 16, 
-         "cg1.4xlarge": 8, "cr1.8xlarge": 16, "c3.large": 1, "c3.xlarge": 2, 
-         "c3.2xlarge": 4, "c3.4xlarge": 8, "c3.8xlarge": 16, "c4.large": 1, 
-         "c4.xlarge": 2, "c4.2xlarge": 4, "c4.4xlarge": 8, "c4.8xlarge": 18, 
-         "hi1.4xlarge": 8, "hs1.8xlarge": 8, "g2.2xlarge": 4, "x1.32xlarge": 64,
-         "r3.large": 1, "r3.xlarge": 2, "r3.2xlarge": 4, "r3.4xlarge": 8, 
-         "r3.8xlarge": 16, "i2.xlarge": 2, "i2.2xlarge": 4, "i2.4xlarge": 8, 
-         "i2.8xlarge": 16, "d2.xlarge": 2, "d2.2xlarge": 4, "d2.4xlarge": 8, 
-         "d2.8xlarge": 18,
-         'g2.2xlarge': 1, 'g2.8xlarge': 4  
-         # Not actual cpus, but gpu concurrency = 4
-         }
+# Get the number of cpus per instance from a text file
+ncpus = {}
 
-def get_all_in_region(region):
+with open("instance_concurrency") as f:
+    for line in f:
+       # print(line)
+       (key, val) = line.split(None)
+       ncpus[key] = int(val)
+
+def get_all_in_region(region, gpu, program):
+    if gpu:
+        all_instances = instances + gpu_instances
+    else:
+        all_instances = instances
+
     client = boto3.client("ec2", region)
     response = client.describe_spot_price_history(
-            InstanceTypes=instances,
+            InstanceTypes=all_instances,
             StartTime=five_days_ago,
             EndTime=now,
             ProductDescriptions=['Linux/UNIX'],
@@ -71,17 +70,19 @@ def get_all_in_region(region):
     prices = np.empty([0, 3])
 
     for item in response["SpotPriceHistory"]:
-        mat = np.array([[item["InstanceType"], item["SpotPrice"], 
+        price = float(item["SpotPrice"])
+
+        mat = np.array([[item["InstanceType"], price, 
             item["AvailabilityZone"]]])
         prices = np.append(prices, mat, axis=0)
 
     return(prices)
 
-def get_all_regions():
+def get_all_regions(gpu, program):
     all_prices = np.empty([0, 3])
     
     for r in regions:
-        mat = get_all_in_region(r)
+        mat = get_all_in_region(r, gpu, program)
         all_prices = np.append(all_prices, mat, axis=0)
 
     return(all_prices)
@@ -109,59 +110,103 @@ def get_all_regions():
 
 #     return([instancename, region, price])
 
-def make_a_cluster(row, num, length):
+def make_a_cluster(row, num, length, program):
 
     cores = ncpus[row[0]]
-    machines = math.ceil(num / cores)
-    price = row[1]
+    price = float(row[1])
+    machines = int(math.ceil(num / cores))
 
-    print(row)
+
+    if re.match("^g", row[0]):
+        length /= gpu_speed_up[program]
+        # print("GPU speed up is " + str(gpu_speed_up[program]))
+
     total_cost = machines * length * price
 
-    return([row[0], row[2], price])
+    return([row[0], row[2], total_cost, machines, length, price, cores])
 
-def best_price(program, length, num, quick, cheapest, gpu=False):
-    """Find the best instance by comparing against previous best."""
+# def best_price(program, length, num, quickest, cheapest, gpu=False):
+#     """Find the best instance by comparing against previous best."""
 
-    print("AWS speed up for " + program + " is " + str(gpu_speed_up[program]) +
-          "x.")
+#     print("AWS speed up for " + program + " is " + str(gpu_speed_up[program]) +
+#           "x.")
 
-    # For comparison, so the first one is always smaller.
-    max_price = sys.maxint
-    max_time = sys.maxint
+#     # For comparison, so the first one is always smaller.
+#     max_price = sys.maxint
+#     max_time = sys.maxint
 
-    if gpu:
-        search_instances = instances + gpu_instances
-    else:
-        search_instances = instances
+#     if gpu:
+#         search_instances = instances + gpu_instances
+#     else:
+#         search_instances = instances
 
-    print("Pricing " + str(len(search_instances)) + " instances against " +
-          str(len(regions)) + " regions ...")
+#     print("Pricing " + str(len(search_instances)) + " instances against " +
+#           str(len(regions)) + " regions ...")
 
-    for r in regions:
-        for i in search_instances:
-            if i in gpu_instances:
-                exec_time = length / gpu_speed_up[program]
-            else:
-                exec_time = length 
+#     for r in regions:
+#         for i in search_instances:
+#             if i in gpu_instances:
+#                 exec_time = length / gpu_speed_up[program]
+#             else:
+#                 exec_time = length 
 
-            cluster = make_a_cluster(price_instance(i, r), num, exec_time)
+#             cluster = make_a_cluster(price_instance(i, r), num, exec_time)
 
-            if cluster[4] < max_price:
-                max_price = cluster[4]
-                best_price = cluster
+#             if cluster[4] < max_price:
+#                 max_price = cluster[4]
+#                 best_price = cluster
 
-            if cluster[5] < max_time:
-                max_time = cluster[5]
-                best_time = cluster
+#             if cluster[5] < max_time:
+#                 max_time = cluster[5]
+#                 best_time = cluster
 
-    print("Format: machine, region, $/instance/hr, #instances, total$, " + 
-        "time(hr)")
+#     print("Format: machine, region, $/instance/hr, #instances, total$, " + 
+#         "time(hr)")
 
 
     # Return appropriate things here
 
-every = get_all_regions()
+def spaces(this_list, tnl=False):
+    '''Print the list spaces for each row.
+    It isn't the prettiest, but it works for debug output'''
+    width = 80 // len(this_list) 
+    fmt = "{:<" + str(width) + "}"
 
-[ make_a_cluster(x, 10, 100) for x in every ]
+    print(''.join([fmt.format(x) for x in this_list]))
 
+    if(tnl):
+        print("")
+
+def get_best_cluster(num, length, gpu, program, showall=False, text=False,
+    showbest=True):
+    every = get_all_regions(gpu=gpu, program=program)
+
+    if showall and showbest:
+        spaces(header)
+
+    maxprice = sys.maxint
+    for x in every:
+        price = make_a_cluster(x, num, length, program)
+
+        if showall:
+            spaces(price)
+
+        if price[2] < maxprice:
+            best = price
+            maxprice = price[2]
+
+    if showall and showbest:
+        spaces(header, tnl=True)
+
+    fmt = '{:<12}{:<20}'
+
+    if showbest:
+        for h,b in zip(header, best):
+            print(fmt.format(str(h), str(b)))
+
+    if text:
+        print("You can run " + str(num) + \
+            " jobs, taking " + str(round(best[6], 4)) + \
+            " hours each on " + str(best[4]) + " " + best[0] + \
+            "-class instances in the " + best[1] + " region. " + \
+            " It will cost you $" + str(round(best[2], 5)) + ".")
